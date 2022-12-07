99a008decebe348b6429141c4303df70cba1915e
Sergey <ser_8904@mail.ru>
2022-12-07 19:37:03 +0300

    Added constraint to make title not nullable

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 class TasksController < ApplicationController
-	respond_to :html, :xml, :json
+  respond_to :html, :xml, :json
 
-	def index
-		@tasks = Task.all
-	end
+  def index
+    @tasks = Task.all
+  end
 end
